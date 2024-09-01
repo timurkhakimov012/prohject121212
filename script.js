438ded8fc8ccb62d7cfb39812a1d5e7a0ee675e5
Timur <examble@gmail.com>
2024-09-01 11:33:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// let money = prompt('Ваш бюджет на месяц?', ''); 
+// let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+// let appData = {
+//     budget:  money,    
+//     timeData: time,
+//     expenses:{} ,
+//     optionalExpenses: {}, 
+//     income: [],
+//     savings: false
+// }
+
+// let question1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+// let question2 = prompt("Во сколько обойдется?", '');
+// let question3 = prompt("Введите обязательную статью расходов в этом месяце", '');
+// let question4 = prompt("Во сколько обойдется?", '');
+
+// appData.expenses.question1 = question2;
+// appData.expenses.question3 = question4;
+
+// alert(appData.budget / 30);
+
+
+
